3634672b937886cb0d1aff794134fca077f9d6bd
gadicandra <garjitaadicandra@mail.ugm.ac.id>
2024-12-06 12:18:22 +0700

    Fix Ps3.h

--- Ps3.h
+++ Ps3.h
@@ -12,16 +12,28 @@ class Ps3 : public Playstation {
     
     public:
         Ps3();
+        //Fungsi untuk mengonstruksi class
+        //Postcondition: ps3RentalRate = 0
 
         Ps3(int rate);
+        //Fungsi untuk mengonstruksi class dengan parameter
+        //Postcondition: ps3RentalRate = rate
 
         int getPS3RentalRate();
+        //Fungsi untuk mengembalikan ps3RentalRate
+        //Postcondition: Nilai ps3RentalRate dikembalikan
 
         int getPS3Availability();
+        //Fungsi untuk mengembalikan jumlah ps3 yang tersedia
+        //Postcondition: Nilai jumlah ps3 yang tersedia dikembalikan
 
         void displayPS3();
+        //Fungsi untuk mencetak ketersediaan dan harga rental ps3
+        //Postcondition: Ketersediaan dan harga rental di cetak di terminal
 
         int countRentalRatePS3(int rentTime);
+        //Fungsi untuk menghitung harga rental
+        //Postcondition: Harga rental dikali dengan lama penyewaan, kemudian mengembalikan nilai perkaliannya
         
 };
 
