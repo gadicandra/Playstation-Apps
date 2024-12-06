a0f66a24196ae3a0de7200476c9f730164eeae49
gadicandra <garjitaadicandra@mail.ugm.ac.id>
2024-12-06 12:39:39 +0700

    Fix UI.h

--- UI.h
+++ UI.h
@@ -17,43 +17,58 @@ class userInterface{
     
     public:
         void header();
-        //Function to display header of the app
+        //Fugsi untuk menampilkan header dari tampilan program
+        //Postcondition: header ditampilkan di terminal
 
         int firstMenu();
-        //Function to display the first menu (Login as Member, signUp, Login as Administrator)
+        //Fungsi untuk menampilkan pilihan(login as member, sign up, atau login as administrator)
+        //Postcondition: Pilihan dari firstmenu akan dikembalikan
 
         int secondMenu();
-        //Function to display the second menu (Profil, Rental PS, Pengembalian Barang, Riwayat Transaksi)
+        //Fungsi untuk menampilkan pilihan(profil, rental ps, pengembalian barang, riwayat transaksi, dan logout)
+        //Postcondition: Pilihan dari secondmenu akan dikembalikan
 
         int menuProfil();
-        //Function to display profil
-
-        void transisiProfil();
-        //Function to transition profil menu to update data or back to second menu
+        //Fungsi untuk menampilkan pilihan(update data, setor balance, dan hapus akun)
+        //Postcondition: Pilihan dari menuprofil akan dikembalikan
 
         int menuRental();
-        //Function to display availability of playstation and the rent price
+        //Fungsi untuk memberi pilihan ps yang akan disewa
+        //Postcondition: Pilihan dari menuRental akan dikembalikan
 
         int konfirmasiPemesanan();
-        //Function to confirmation the rent
+        //Fungsi untuk mengonfirmasi ulang apakah akan membuat pesanan
+        //Postcondition: Pilihan konfirmasi akan dikembalikan
 
         void balanceKurang();
-        //Function to display when balance is not enough to pay
+        //Fungsi untuk menampilkan "balance anda kurang" jika balance kurang
+        //Postcondition: "Balance anda kurang" ditampilkan di terminal
+
 
         void pembayaranBerhasil();
-        //Function to display when transaction is succesfull
+        //Fungsi untuk menampilkan "Pembayaran berhasil"
+        //Postcondition: "Pembayaran berhasil" ditampilkan di terminal
 
         int menuPengembalian();
-        //Funtion to display return playstation menu
+        //Fungsi untuk memilih playstation yang ingin dikembalikan
+        //Postcondition: Mengembalikan pilihan ps yang akan dikembalikan
 
         int konfirmasiPengembalian();
+        //Fungsi untuk menampilkan pilihan yang mengonfirmasi pengembalian
+        //Postcondition: Mengembalikan pilihan konfirmasi;
 
         void menuKembali();
-        //Function to display transaction history
+        //Fungsi untuk kembali ke menu sebelumnya
+        //Postcondition: Mengambil input sembarang kemudian mengembalikan ke menu sebelumnya
 
         int menuAdministrator();
+        //Fungsi untuk menampilkan pilihan(jumlah pendapatan, mengatur ketersediaan, 
+        //konfirmasi pengembalian, list terpesan, list user terdaftar, dan logout)
+        //Postcondition: Mengembalikan pilihan menuAdministrator
 
         void ringkasan();
+        //Fungsi untuk menampilkan ringkasan untuk admin
+        //Postcondition: Ringkasan ditampilkan di terminal
 
 };
 
