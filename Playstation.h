a35396380b514bb2548a5f6cdacf4be5a9e2fcf8
gadicandra <garjitaadicandra@mail.ugm.ac.id>
2024-12-06 12:00:13 +0700

    Playstation.h Fix

--- Playstation.h
+++ Playstation.h
@@ -10,31 +10,45 @@ class Playstation: public userInterface{
         int pilihanKonfirmasi;
     public:
         int pemesanan();
+        //Fungsi untuk memilih playstation yang akan dipesan dan mengembalikan pilihannya
 
         bool cekBalance(string us, int rent);
+        //Fungsi untuk mengecek apakah balance cukup untuk menyewa playstation
 
         void mengurangiBalance(string us, int rent);
+        //Fungsi untuk mengurangi balance jika mengonfirmasi akan menyewa playstation
 
         int lamaPenyewaan();
+        //Fungsi untuk menginput berapa lama penyewaan dan mengembalikan nilainya
 
         void pesanan(int ps, int rent, int time, string us);
+        //Fungsi untuk mencetak pesanan di history.txt dan listPesanan.txt
 
         void mengurangiKetersediaan(int ps);
+        //Fungsi untuk mengurangi ketersediaan ps setelah mengonfirmasi penyewaan
 
         void mengaturKetersediaan();
+        //Fungsi untuk mengatur ketersediaan ps yang dilakukan oleh Admin
 
         bool pengembalianPS(int ps, string us);
+        //Fungsi untuk mengecek apakah terdapat ps(3/4/5) yang belum dikembalikan
 
         bool cekKetersediaan(int ps);
+        //Fungsi untuk mengecek apakah ps tersedia untuk di sewa
 
         void riwayatPemesanan(string us);
+        //Fungsi untuk mengecek riwayat pemesanan yang dilakukan oleh member
 
         void ringkasanAdmin(int pilihan, int rent, int time);
+        //Fungsi untuk mencetak ringkasan untuk Admin
 
         void konfirmasi(int ps, string us);
+        //Fungsi untuk meminta konfirmasi pengembalian ke Admin
 
         void konfirmasiAdmin();
+        //Fungsi untuk mengkonfirmasi pengembalian oleh Admin
 
         void listPesanan();
+        //Fungsi untuk mencetak penyewaan yang sedang berjalan
 };
 #endif //PLAYSTATION_H
