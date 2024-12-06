5ae40b5cf6b5e204708249a03a54100de0bf9ef2
gadicandra <garjitaadicandra@mail.ugm.ac.id>
2024-12-06 12:18:59 +0700

    Fix Ps4.h

--- Ps4.h
+++ Ps4.h
@@ -13,16 +13,29 @@ class Ps4 : public Playstation {
     
     public:
         Ps4();
+        //Fungsi untuk mengonstruksi class
+        //Postcondition: ps4RentalRate = 0
 
         Ps4(int rate);
+        //Fungsi untuk mengonstruksi class dengan parameter
+        //Postcondition: ps4RentalRate = rate
 
         int getPS4RentalRate();
+        //Fungsi untuk mengembalikan ps4RentalRate
+        //Postcondition: Nilai ps4RentalRate dikembalikan
 
         int getPS4Availability();
+        //Fungsi untuk mengembalikan jumlah ps4 yang tersedia
+        //Postcondition: Nilai jumlah ps4 yang tersedia dikembalikan
 
         void displayPS4();
+        //Fungsi untuk mencetak ketersediaan dan harga rental ps4
+        //Postcondition: Ketersediaan dan harga rental di cetak di terminal
 
         int countRentalRatePS4(int rentTime);
+        //Fungsi untuk menghitung harga rental
+        //Postcondition: Harga rental dikali dengan lama penyewaan, kemudian mengembalikan nilai perkaliannya
+        
 };
 
 #endif //PS4_H
